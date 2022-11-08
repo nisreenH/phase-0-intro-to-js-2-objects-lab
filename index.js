0ffe0167ec5d147a554a9ce4af284036c27b12b4
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-11-08 12:04:13 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+let employee = {
+    name : "Nisreen",
+    streetAddress : "Main street"
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    const newObj = { ...obj }
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]
+    return obj
+}
